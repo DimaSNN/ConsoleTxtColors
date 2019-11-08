30235f63e6cf7ccb710c5e961095c689692fa548
Sidorenkov Dmitry <dima-sidorenkov@mail.ru>
2019-11-08 10:38:51 +0300

    init commit
    Small test programm

---
+++ main.cpp
@@ -0,0 +1,41 @@
+//This may be in a header file
+#ifndef _COLORS_
+#define _COLORS_
+
+/* FOREGROUND */
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLD(x) "\x1B[1m" x RST
+#define UNDL(x) "\x1B[4m" x RST
+
+#endif  /* _COLORS_ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << FRED("I'm red.") << endl;
+    cout << UNDL(FRED("I'm red-undl.")) << endl;
+    cout << BOLD(FRED("I'm red-bold.")) << endl;
+    cout << FBLU("I'm blue.") << endl;
+    
+    return 0;
+}
